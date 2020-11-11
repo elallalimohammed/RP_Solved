d99264c4850560d202b3e0ff7a142a76a0b7769d
Mohammed El Allali <moal@easj.dk>
2020-11-11 13:48:51 +0100

    Adding Book_Store V10

---
+++ RazorPages_Solutions/Book_StoreV10/Book_StoreV10/Pages/Orders/CheckOut.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Book_StoreV10.Models;
+using Book_StoreV10.Repositories;
+using Book_StoreV10.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Book_StoreV10
+{
+    public class CheckOutModel : PageModel
+    {
+       private JsonOrderRepository repository;
+        private ShoppingCartService cart;
+        
+        [BindProperty]
+        public Student Student { get; set; }   
+        public Order Order { get; set; }
+
+        public CheckOutModel(JsonOrderRepository repo, ShoppingCartService cartService)
+        {
+            repository = repo;
+            cart = cartService;
+        }
+        public void OnGet()
+        {
+            //Student s = Student;
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            Order order = new Order();
+            order.OrderID = 12;
+            order.Student = Student;
+            order.Books =  cart.GetOrderedBooks();
+            order.DateTime= DateTime.Now ;
+            repository.AddOrder(order);
+            return RedirectToPage("Order", Student);
+        }
+    }
+}
---
+++ RazorPages_Solutions/Book_StoreV10/Book_StoreV10/Pages/Orders/Order.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Book_StoreV10.Models;
+using Book_StoreV10.Repositories;
+using Book_StoreV10.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Book_StoreV10
+{
+    public class OrderModel : PageModel
+    {
+        private JsonOrderRepository repository;
+
+        private ShoppingCartService cart;
+
+        public Student Student { get; set; }
+        public Order Order { get; set; }   
+        public List<Book> cartItems { get; set; }
+
+        public OrderModel(JsonOrderRepository repoService, ShoppingCartService cartService)
+        {
+            repository = repoService;
+            cart = cartService;
+        }
+
+        public IActionResult OnGet(Student st)
+        {
+            Student = st;              
+            cartItems = cart.GetOrderedBooks();
+            if (cartItems?.Count() <= 0)
+            {
+                return Redirect("ShoppingCart");
+            }
+            return Page();
+        }
+    }
+}
